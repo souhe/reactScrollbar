058fbf14c3001aa7813e483bc40d9bab8e84c5cd
souhe <souhepend@hotmail.com>
2015-04-17 08:40:58 +0200

    ScrollBar are moving now

--- src/js/scrollArea.jsx
+++ src/js/scrollArea.jsx
@@ -2,20 +2,58 @@ import React from 'react';
 import Scrollbar from './scrollBar.jsx';
 
 class ScrollArea extends React.Component{
+    constructor(props){
+        this.state = {
+            topPosition: 0,
+            realHeight: 0,
+            containerHeight: 0
+        };
+    }
+
+    componentDidMount(){
+        var content = React.findDOMNode(this.refs.content);
+        var container = React.findDOMNode(this);
+
+        this.setState({
+            realHeight: content.offsetHeight,
+            containerHeight: container.offsetHeight
+        })
+    }
+
     render(){
+        var style = {
+            marginTop: this.state.topPosition
+        };
+
         return (
             <div className={this.props.className} onWheel={this.handleWheel.bind(this)}>
-                <div ref="content">
+                <div ref="content" style={style}>
                     {this.props.children}
                 </div>
-                <Scrollbar />
+                <Scrollbar
+                    realHeight={this.state.realHeight}
+                    containerHeight={this.state.containerHeight}
+                    scrollTop={-this.state.topPosition}
+                    onMove={this.handleMove.bind(this)}/>
             </div>
         );
     }
 
+    handleMove(topPosition){
+        console.log('cont top pos', topPosition);
+        this.setState({topPosition});
+    }
+
     handleWheel(x){
-        var element = React.findDOMNode(this);
-        element.scrollTop += x.deltaY;
+        var element = React.findDOMNode(this.refs.content);
+        // element.margin += x.deltaY;
+        var newTopPosition = this.state.topPosition - x.deltaY;
+        if(-newTopPosition > this.state.realHeight - this.state.containerHeight){
+            newTopPosition = -(this.state.realHeight - this.state.containerHeight);
+        } else if(newTopPosition > 0){
+            newTopPosition = 0;
+        }
+        this.setState({topPosition: newTopPosition});
     }
 }
 
--- src/js/scrollBar.jsx
+++ src/js/scrollBar.jsx
@@ -1,12 +1,67 @@
 import React from 'react';
 
 class ScrollBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            topPosition: 0,
+            scrollHeight: 0,
+            isDragging: false,
+            lastClientY: 0
+        }
+    }
+
+    componentWillReceiveProps(nextProps){
+        this.setState(this.calculateState(nextProps));
+    }
+
+    calculateState(props){
+        var scrollHeight = props.containerHeight * props.containerHeight / props.realHeight;
+        var multiplier = props.containerHeight / props.realHeight;
+        var topPosition = props.scrollTop * multiplier;
+
+        return {
+            scrollHeight: scrollHeight,
+            topPosition: topPosition
+        };
+    }
+
     render(){
+        var scrollStyle = {
+            height: this.state.scrollHeight,
+            marginTop: this.state.topPosition
+        };
         return (
-            <div className="scroll-container">
-                <div className="scroll"></div>
+            <div className="scroll-container"
+                onMouseMove={this.handleMouseMove.bind(this)} >
+                <div className="scroll"
+                    style={scrollStyle}
+                    onMouseDown={this.handleMouseDown.bind(this)}
+                    onMouseUp={this.handleMouseUp.bind(this)} >
+
+                </div>
             </div>
         );
+    }
+
+    handleMouseMove(e){
+        if(this.state.isDragging){
+            var multiplier = this.props.containerHeight / this.props.realHeight;
+            var deltaY = this.state.lastClientY - e.clientY;
+            var newTopPosition = this.state.topPosition - deltaY;
+            console.log("new position", newTopPosition);
+
+            this.setState({ lastClientY: e.clientY });
+            this.props.onMove(newTopPosition/multiplier);
+        }
+    }
+
+    handleMouseDown(e){
+        this.setState({isDragging: true, lastClientY: e.clientY });
+    }
+
+    handleMouseUp(e){
+        this.setState({isDragging: false });
     }
 }
 
