d2408cb4939e1006e8e7ab8b7e33ab1b4a2e8e73
souhe <souhepend@hotmail.com>
2016-01-29 23:37:45 +0100

    More tests

--- test/scrollArea.spec.js
+++ test/scrollArea.spec.js
@@ -15,7 +15,7 @@ function setup(props){
     let wrapper = output.props.children();
 
     let content = wrapper.props.children[0];
-    let scrollbars = wrapper.props.children.filter(element => element.type == Scrollbar);
+    let scrollbars = wrapper.props.children.filter(element => element && element.type == Scrollbar);
     
     return {
         wrapper,
@@ -26,8 +26,8 @@ function setup(props){
     };
 }
 
-describe('ScrolLArea', () => {
-   it('Should render children and both scrollbars', () => {      
+describe('ScrolLArea component', () => {
+    it('Should render children and both scrollbars', () => {      
         let {scrollbars, content} = setup();
 
         expect(scrollbars.length).toBe(2);
@@ -40,5 +40,47 @@ describe('ScrolLArea', () => {
                 onTouchEnd={() => {}}>
                 <p>content</p>
             </div>);
+    });
+   
+    it('Could render only vertical scrollbar', () => {
+        let {scrollbars} = setup({vertical: true, horizontal: false});
+        let scrollbar = scrollbars[0];
+        
+        expect(scrollbars.length).toBe(1);
+        expect(scrollbar.props.type).toBe('vertical'); 
+    });
+   
+    it('Could render only horizontal scrollbar', () => {
+        let {scrollbars} = setup({vertical: false, horizontal: true});
+        let scrollbar = scrollbars[0];
+        
+        expect(scrollbars.length).toBe(1);
+        expect(scrollbar.props.type).toBe('horizontal'); 
+   });
+   
+   it('Should change content element class when contentClassName porp is used', () => {
+        let {content} = setup({contentClassName: 'test-class'});
+        
+        expect(content.props.className).toInclude('test-class');
+   });
+   
+   it('Should have proper scrollbars styles', () => {
+        let {content, scrollbars} = setup({
+            vertical: true,
+            verticalScrollbarStyle: {test: 'verticalScrollbarStyle'},
+            verticalContainerStyle: {test: 'verticalContainerStyle'},
+            horizontal: true,
+            horizontalScrollbarStyle: {test: 'horizontalScrollbarStyle'},
+            horizontalContainerStyle: {test: 'horizontalContainerStyle'}
+        });
+        
+        let verticalScrollbar = scrollbars.filter(component => component.props.type === 'vertical')[0];
+        let horizontalScrollbar = scrollbars.filter(component => component.props.type === 'horizontal')[0];
+        
+        expect(verticalScrollbar.props.containerStyle).toEqual({test: 'verticalContainerStyle'});
+        expect(verticalScrollbar.props.scrollbarStyle).toEqual({test: 'verticalScrollbarStyle'});
+        expect(horizontalScrollbar.props.containerStyle).toEqual({test: 'horizontalContainerStyle'});
+        expect(horizontalScrollbar.props.scrollbarStyle).toEqual({test: 'horizontalScrollbarStyle'});
+            
    });
 });
