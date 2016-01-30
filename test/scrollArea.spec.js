6702283c50f050c898f044de87b2d00cdb841432
souhe <souhepend@hotmail.com>
2016-01-30 19:31:28 +0100

    More ScrollArea tests

--- test/scrollArea.spec.js
+++ test/scrollArea.spec.js
@@ -3,6 +3,7 @@ import expectJSX from 'expect-jsx';
 expect.extend(expectJSX);
 
 import React, { Component } from 'react';
+import {render} from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import ScrollArea from '../src/js/scrollArea';
 import Scrollbar from '../src/js/scrollBar';
@@ -10,6 +11,7 @@ import Scrollbar from '../src/js/scrollBar';
 function setup(props){
     let renderer = TestUtils.createRenderer();
     renderer.render(<ScrollArea {...props}><p>content</p></ScrollArea>);
+    let instance = getRendererComponentInstance(renderer);
     let output = renderer.getRenderOutput();
 
     let wrapper = output.props.children();
@@ -22,8 +24,27 @@ function setup(props){
         content,
         scrollbars,
         renderer,
-        output
+        output,
+        instance
     };
+}
+
+function setupComponentWithMockedSizes(props) {
+    let component = setup(props);
+    component.instance.computeSizes = () => ({
+        realHeight: 300,
+        containerHeight: 100,
+        realWidth: 300,
+        containerWidth: 100
+    });
+    
+    component.instance.setSizesToState();
+    
+    return component;
+}
+
+function getRendererComponentInstance(renderer){
+    return renderer._instance? renderer._instance._instance : null;
 }
 
 describe('ScrolLArea component', () => {
@@ -56,15 +77,15 @@ describe('ScrolLArea component', () => {
         
         expect(scrollbars.length).toBe(1);
         expect(scrollbar.props.type).toBe('horizontal'); 
-   });
+    });
    
-   it('Should change content element class when contentClassName porp is used', () => {
+    it('Should change content element class when contentClassName porp is used', () => {
         let {content} = setup({contentClassName: 'test-class'});
         
         expect(content.props.className).toInclude('test-class');
-   });
+    });
    
-   it('Should have proper scrollbars styles', () => {
+    it('Should have proper scrollbars styles', () => {
         let {content, scrollbars} = setup({
             vertical: true,
             verticalScrollbarStyle: {test: 'verticalScrollbarStyle'},
@@ -81,6 +102,87 @@ describe('ScrolLArea component', () => {
         expect(verticalScrollbar.props.scrollbarStyle).toEqual({test: 'verticalScrollbarStyle'});
         expect(horizontalScrollbar.props.containerStyle).toEqual({test: 'horizontalContainerStyle'});
         expect(horizontalScrollbar.props.scrollbarStyle).toEqual({test: 'horizontalScrollbarStyle'});
-            
-   });
+    });
+   
+    it('normalizeTopPosition() shoud returns proper value', () => {
+        let {    instance} = setup();
+        let {normalizeTopPosition} = instance;
+        let sizes = {realHeight: 30, containerHeight: 20};
+       
+        expect(normalizeTopPosition(0, sizes)).toBe(0);
+        expect(normalizeTopPosition(-5, sizes)).toBe(-5);
+        expect(normalizeTopPosition(-10, sizes)).toBe(-10);
+        expect(normalizeTopPosition(-11, sizes)).toBe(-10);
+        expect(normalizeTopPosition(1, sizes)).toBe(0);
+        expect(normalizeTopPosition(60, sizes)).toBe(0);
+        expect(normalizeTopPosition(-100, sizes)).toBe(-10);
+    });
+    
+    it('normalizeLeftPosition() shoud returns proper value', () => {
+        let {instance} = setup();
+        let {normalizeLeftPosition} = instance;
+        let sizes = {realWidth: 30, containerWidth: 20};
+        
+        expect(normalizeLeftPosition(0, sizes)).toBe(0);
+        expect(normalizeLeftPosition(-5, sizes)).toBe(-5);
+        expect(normalizeLeftPosition(-10, sizes)).toBe(-10);
+        expect(normalizeLeftPosition(-11, sizes)).toBe(-10);
+        expect(normalizeLeftPosition(1, sizes)).toBe(0);
+        expect(normalizeLeftPosition(60, sizes)).toBe(0);
+        expect(normalizeLeftPosition(-100, sizes)).toBe(-10);
+    });
+    
+    it('handleWheel method work properly when scrolling down', () => {
+        let {instance} = setupComponentWithMockedSizes();
+        let e = {deltaY:20, deltaX: 0, preventDefault: () => {}};      
+        instance.handleWheel(e);
+        
+        expect(instance.state.topPosition).toBe(-20);
+    });
+    
+    it('handleWheel method work properly when scrolling up and actual topPosition is 0', () => {
+        let {instance} = setupComponentWithMockedSizes();
+        let e = {deltaY:-10, deltaX: 0, preventDefault: () => {}};      
+        instance.handleWheel(e);
+        
+        expect(instance.state.topPosition).toBe(0);
+    });
+    
+    it('handleWheel method work properly when scrolling down more then content height', () => {
+        let {instance} = setupComponentWithMockedSizes();
+        
+        for(let i = 0; i < 10; i++){
+            let e = {deltaY:50, deltaX: 0, preventDefault: () => {}};      
+            instance.handleWheel(e);
+        }        
+        
+        expect(instance.state.topPosition).toBe(-200);
+    });
+    
+    it('handleWheel method work properly when scrolling right', () => {
+        let {instance} = setupComponentWithMockedSizes();
+        let e = {deltaY:0, deltaX: 20, preventDefault: () => {}};  
+        instance.handleWheel(e);
+
+        expect(instance.state.leftPosition).toBe(-20);
+    });
+    
+    it('handleWheel method work properly when scrolling left and actual leftPosition is 0', () => {
+        let {instance} = setupComponentWithMockedSizes();
+        let e = {deltaY:0, deltaX: -10, preventDefault: () => {}};      
+        instance.handleWheel(e);
+        
+        expect(instance.state.leftPosition).toBe(0);
+    });
+    
+    it('handleWheel method work properly when scrolling right more then content height', () => {
+        let {instance} = setupComponentWithMockedSizes();
+        
+        for(let i = 0; i < 10; i++){
+            let e = {deltaY:0, deltaX: 50, preventDefault: () => {}};      
+            instance.handleWheel(e);
+        }        
+        
+        expect(instance.state.leftPosition).toBe(-200);
+    });
 });
