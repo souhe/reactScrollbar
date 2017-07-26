bbbc15896dfa961a92293f29b2a3fe0dbd1b9991
Oleg Sorokin <cnt2box@gmail.com>
2017-07-26 13:49:07 +0500

    Allow arrows keys for textarea element.

--- src/js/ScrollArea.jsx
+++ src/js/ScrollArea.jsx
@@ -274,7 +274,7 @@ export default class ScrollArea extends React.Component {
 
     handleKeyDown(e) {
         // only handle if scroll area is in focus
-        if (e.target.tagName.toLowerCase() !== 'input') {
+        if (e.target.tagName.toLowerCase() !== 'input' && e.target.tagName.toLowerCase() !== 'textarea') {
             let deltaY = 0;
             let deltaX = 0;
             let lineHeight = this.lineHeightPx ? this.lineHeightPx : 10;
